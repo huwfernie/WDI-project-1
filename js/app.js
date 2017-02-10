1b12c1eac6d421a3cd37a9ea1019b87fd69f9535
Huw Fernie <HuwFernie@gmail.com>
2017-02-10 13:47:33 +0000

    one square with some response to clicks

--- js/app.js
+++ js/app.js
@@ -1,7 +1,50 @@
 console.log('Hello Huw');
 
 
+
+
 $(() => {
 // run this on load!!
+  const $active = $('.active');
+  const $reset = $('#resetButton');
+
+  // create an anon. function for each click on the sideBar
+  // and top-bar
+  $active.on('click', function(e) {
+    console.log(`clickeroo`);
+    if (amIActive(e)) {
+      $(e.target).addClass('clicked');
+      $(e.target).removeClass('active');
+    } else {
+      //
+    }// end of amIActive
+  });// end of $active.on(...
+
+  $reset.on('click', function() {
+    console.log(`reset button`);
+  });
+
+
+
+  // All the functions now! -------------------------------
+
+  function amIActive(e) {
+    if ($(e.target).hasClass('active')) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
 
 });
